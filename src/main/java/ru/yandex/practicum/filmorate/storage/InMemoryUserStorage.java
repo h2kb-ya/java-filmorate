899b81ff409bd3b25d233d3af4a5092168625fa7
michailb <michailb@kmslh.com>
2024-11-02 15:53:48 +0300

    controllers-films-users
    - Remove unused imports

--- src/main/java/ru/yandex/practicum/filmorate/storage/InMemoryUserStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/InMemoryUserStorage.java
@@ -6,7 +6,6 @@ import java.util.Map;
 import java.util.Optional;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
-import ru.yandex.practicum.filmorate.exception.NotFoundException;
 import ru.yandex.practicum.filmorate.model.User;
 
 @Slf4j
