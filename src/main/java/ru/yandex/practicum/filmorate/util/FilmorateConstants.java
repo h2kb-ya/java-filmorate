1b5a3e7a623c79662cda79d67a4f98f04bec0cf8
michailb <michailb@kmslh.com>
2024-11-05 10:09:51 +0300

    controllers-films-users
    - Fix after MR review

--- src/main/java/ru/yandex/practicum/filmorate/util/FilmorateConstants.java
+++ src/main/java/ru/yandex/practicum/filmorate/util/FilmorateConstants.java
@@ -4,5 +4,6 @@ import lombok.experimental.UtilityClass;
 
 @UtilityClass
 public class FilmorateConstants {
-    public final String DEFAULT_COUNT_VALUE_FOR_GETTING_POPULAR_FILMS = "10";
+
+    public static final String DEFAULT_COUNT_VALUE_FOR_GETTING_POPULAR_FILMS = "10";
 }
