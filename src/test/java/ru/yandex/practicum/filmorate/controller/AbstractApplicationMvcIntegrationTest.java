b92b6af97a766575d2f6659adf9b686db8de4307
michailb <michailb@kmslh.com>
2024-12-25 10:09:00 +0300

    sprint12-middle-check
    - remove unused imports

--- src/test/java/ru/yandex/practicum/filmorate/controller/AbstractApplicationMvcIntegrationTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/controller/AbstractApplicationMvcIntegrationTest.java
@@ -9,7 +9,6 @@ import lombok.SneakyThrows;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 
