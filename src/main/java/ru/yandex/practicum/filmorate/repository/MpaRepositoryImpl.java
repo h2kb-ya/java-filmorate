4844139b3829e67cf712a361f097a0d5730e6cc2
michailb <michailb@kmslh.com>
2024-12-26 15:32:58 +0300

    add-database
    - Remove unused imports

--- src/main/java/ru/yandex/practicum/filmorate/repository/MpaRepositoryImpl.java
+++ src/main/java/ru/yandex/practicum/filmorate/repository/MpaRepositoryImpl.java
@@ -5,7 +5,6 @@ import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.dao.EmptyResultDataAccessException;
-import org.springframework.data.relational.core.sql.In;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 import ru.yandex.practicum.filmorate.model.Mpa;
