762fa57e321dc947c7faa7fc25aea544162ee111
kkn365 <KvitchenkoKN@yandex.ru>
2025-01-14 00:13:19 +0300

    Добавлен контроллер и dal.

---
+++ main/src/main/java/ru/yandex/practicum/filmorate/service/ReviewService.java
@@ -0,0 +1,19 @@
+package ru.yandex.practicum.filmorate.service;
+
+import ru.yandex.practicum.filmorate.model.Review;
+
+import java.util.Collection;
+
+public interface ReviewService {
+
+    Review addNewReview(Review newReview);
+    Review updateCurrentReview(Review currentReview);
+    Review deleteCurrentReview(Integer reviewId);
+    Review getReviewById(Integer reviewId);
+    Collection<Review> getReviewsListByFilmId(Integer filmId, Integer count);
+    Review putLikeToReview(Integer reviewId, Integer userId);
+    Review putDislikeToReview(Integer reviewId, Integer userId);
+    Review deleteLikeFromReview(Integer reviewId, Integer userId);
+    Review deleteDislikeFromReview(Integer reviewId, Integer userId);
+
+}
